ae5ef628670152087f65864f7f0db6eff6ac4ddc
Sofía <sofimaturana830@gmail.com>
2025-11-20 10:48:50 -0300

    Remove contents of _site

--- _site/assets/js/helper.js
+++
@@ -1,9 +0,0 @@
-/// <reference no-default-lib="true" />
-/// <reference lib="dom" />
-const dark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-
-if (dark) {
-  document.documentElement.setAttribute("data-theme", "sunset");
-} else {
-  document.documentElement.setAttribute("data-theme", "light");
-}
